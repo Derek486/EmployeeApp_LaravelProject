7862c41977418c14f2ff821174eefb77ad77010d
Abdieldixon22 <juanjosecondoripinto@gmail.com>
2023-05-20 00:42:22 -0500

    first commit - migrations and template controller

---
+++ app/Http/Controllers/EmployeeController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class EmployeeController extends Controller
+{
+    /**
+     * functions
+     * index -> mostrar todos los empleados
+     * store -> guardar empleado
+     * delete -> eliminar empleado
+     * show -> mostrar empleado para actualizar
+     * update -> actualizar empleado
+     */
+    public function index() {
+        
+    }
+    public function store(Request $request) {
+
+    }
+    public function delete($id) {
+
+    }
+    public function show(Request $request) {
+
+    }
+    public function update(Request $request) {
+
+    }
+}
